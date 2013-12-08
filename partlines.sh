c27f6378f31af7fbb805ae336e3b3f34130c8418
Brian Morearty <brian@morearty.org>
2013-12-08 12:30:48 -0800

    Add Dockerfile and comments

--- partlines.sh
+++ partlines.sh
@@ -1,3 +1,28 @@
+# This file demonstrates a problem that was introduced between Docker 0.6.6 and 0.7.1.
+#
+# To reproduce:
+# 1. Make a Dockerfile that RUNs something that prints partial lines (without LF) to stdout,
+#    then prints a subsequent line feed. (This script does that. So does Ruby's `bundle install` command).
+# 2. docker build
+#
+# RESULT:   `docker build` prints each line, but then replaces it with a blank line when
+#           the LF is printed. For example, after the first two echoes have executed
+#           and while the third command is still waiting, the output looks like this
+#           (underscore represents the cursor location):
+#
+#             $ docker build .
+#
+#
+#             _Doing the last bit of work...
+#
+# EXPECTED: The previous lines of output should not be cleared:
+#
+#             $ docker build .
+#             Doing some work...
+#             Doing some more work...
+#             Doing the last bit of work..._
+#
+
 echo -n Doing some work... 
 sleep 1
 echo
